accd18cbdf882c93bc6735c3ca914150f3557ef3
johnlindquist <johnlindquist@gmail.com>
2012-10-11 16:14:40 -0600

    first pages commit

---
+++ pv3d.disqus.com/disqus7c9d.js
@@ -0,0 +1,14 @@
+var disqus_domain = 'disqus.com';
+var disqus_shortname = 'pv3d';
+var disqus_thread_slug = 'test_if_a_plane_is_within_the_view_of_the_camera_aka_testing_if_culled' || undefined;
+
+(function () {
+  var script = document.createElement('script');
+  script.src = 'http://disqus.com/forums/pv3d/embed.js';
+
+  (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(script);
+
+  // The WordPress plugin has placeholders for comments, so we need to
+  // clear them out.
+  document.getElementById('disqus_thread').innerHTML = '';
+}());
